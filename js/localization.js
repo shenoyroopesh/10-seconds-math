cb6e3879b8f541b05f461d0610047e4efbb49599
michaeljakob <michael@jakob.tv>
2014-09-10 15:32:09 +0200

    Disabled localization

--- js/localization.js
+++ js/localization.js
@@ -1,34 +1,34 @@
 
 $(function() {
-    function getLanguage() {
-      var language = window.navigator.userLanguage || window.navigator.language;
-      return language;
-    }
+    // function getLanguage() {
+    //   var language = window.navigator.userLanguage || window.navigator.language;
+    //   return language;
+    // }
 
-    var $title = $('#text-title');
-    var $selectrange = $('#text-selectrange');
-    var $operations = $('#text-operations');
+    // var $title = $('#text-title');
+    // var $selectrange = $('#text-selectrange');
+    // var $operations = $('#text-operations');
 
 
-    var $opAdd = $('#text-op-add');
-    var $opSub = $('#text-op-sub');
-    var $opMul = $('#text-op-mul');
-    var $opDiv = $('#text-op-div');
-    var $opPow = $('#text-op-pow');
+    // var $opAdd = $('#text-op-add');
+    // var $opSub = $('#text-op-sub');
+    // var $opMul = $('#text-op-mul');
+    // var $opDiv = $('#text-op-div');
+    // var $opPow = $('#text-op-pow');
 
-    var $answer = $('#question-answer');
+    // var $answer = $('#question-answer');
 
-    if (getLanguage().indexOf('de') !== -1) {
-        $title.text("Kopfrechentrainer");
-        $selectrange.text("Wertebereich");
-        $operations.text("Rechenarten");
+    // if (getLanguage().indexOf('de') !== -1) {
+    //     $title.text("Kopfrechentrainer");
+    //     $selectrange.text("Wertebereich");
+    //     $operations.text("Rechenarten");
 
-        $opAdd.text("Addieren");
-        $opSub.text("Subtrahieren");
-        $opMul.text("Multiplizieren");
-        $opDiv.text("Dividieren");
-        $opPow.text("Potenzen");
+    //     $opAdd.text("Addieren");
+    //     $opSub.text("Subtrahieren");
+    //     $opMul.text("Multiplizieren");
+    //     $opDiv.text("Dividieren");
+    //     $opPow.text("Potenzen");
 
-        $answer.text("Antwort");
-    }
+    //     $answer.text("Antwort");
+    // }
 });
